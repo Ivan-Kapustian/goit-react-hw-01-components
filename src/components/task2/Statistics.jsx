8c536579bc46df1818a98bd9528c3a5e57ddd2b6
Ivan Kapustian <michiro2845@gmail.com>
2023-08-30 01:42:17 +0300

    task2 done

--- src/components/task2/Statistics.jsx
+++ src/components/task2/Statistics.jsx
@@ -1,16 +1,20 @@
 import PropTypes from 'prop-types';
 import css from 'components/task2/Statistics.module.css';
-
+import { getRandomColor } from 'components/utils/random-color';
 export const Statistics = ({ title, stats }) => {
   return (
-    <section className="statistics">
-      <h2 className="title">{title}</h2>
+    <section className={css.statistics}>
+      <h2 className={css.title}>{title}</h2>
 
-      <ul className="stat-list">
+      <ul className={css.statList}>
         {stats.map(stat => (
-          <li key={stat.id} className="item">
-            <span className="label">{stat.label}</span>
-            <span className="percentage">{stat.percentage}</span>
+          <li
+            key={stat.id}
+            className={css.item}
+            style={{ backgroundColor: getRandomColor() }}
+          >
+            <span className={css.label}>{stat.label}</span>
+            <span className={css.percentage}>{stat.percentage}%</span>
           </li>
         ))}
       </ul>
@@ -26,5 +30,5 @@ Statistics.propTypes = {
       label: PropTypes.string.isRequired,
       percentage: PropTypes.number.isRequired,
     })
-  ),
+  ).isRequired,
 };
