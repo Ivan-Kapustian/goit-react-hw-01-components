0b438932b0a8578a06739c87442e92f2e3e24303
Ivan Kapustian <michiro2845@gmail.com>
2023-08-30 16:37:35 +0300

    onemore fix

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,7 +4,7 @@ import { getRandomColor } from 'components/utils/random-color';
 export const Statistics = ({ title, stats }) => {
   return (
     <section className={css.statistics}>
-      <h2 className={css.title}>{title && 'Upload stats'}</h2>
+      {title && <h2 className={css.title}>{title}</h2>}
 
       <ul className={css.statList}>
         {stats.map(stat => (
