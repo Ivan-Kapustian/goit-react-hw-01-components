82d5d02de9ab6141c7aa1b2c60c543357e614a61
Ivan Kapustian <michiro2845@gmail.com>
2023-08-30 16:24:52 +0300

    hw fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Profile } from 'components/task1/Profile';
-import { Statistics } from 'components/task2/Statistics';
-import { FriendList } from 'components/task3/FriendList';
-import { TransactionHistory } from 'components/task4/TransactionHistory';
+import { Profile } from 'components/Profile/Profile';
+import { Statistics } from 'components/Statistics/Statistics';
+import { FriendList } from 'components/FriendList/FriendList';
+import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
 import user from 'data/user';
 import data from 'data/data';
 import friends from 'data/friends.json';
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
-import css from 'components/task2/Statistics.module.css';
+import css from 'components/Statistics/Statistics.module.css';
 import { getRandomColor } from 'components/utils/random-color';
 export const Statistics = ({ title, stats }) => {
   return (
     <section className={css.statistics}>
-      <h2 className={css.title}>{title}</h2>
+      <h2 className={css.title}>{title && 'Upload stats'}</h2>
 
       <ul className={css.statList}>
         {stats.map(stat => (
